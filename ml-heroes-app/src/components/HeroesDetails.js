d39dcf60d24c6f86edfe2954f5cf3eac629cbe7f
yandiar12 <yandiar.rohman@gmail.com>
2021-10-18 18:28:39 +0700

    styling

--- ml-heroes-app/src/components/HeroesDetails.js
+++ ml-heroes-app/src/components/HeroesDetails.js
@@ -18,10 +18,12 @@ const HeroesDetiails = () => {
         <div key={id} className="heroes-preview">
           <h1>{ filteredHero.name }</h1>
           <p>Role: { filteredHero.role }</p>
-          <h5>Story:</h5>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo officiis ipsam repudiandae amet error eligendi impedit illo sunt in nobis ad ut doloribus repellat, delectus nihil laborum quas sed eum.</p>
+          <div className="story">
+            <h5>Story:</h5>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo officiis ipsam repudiandae amet error eligendi impedit illo sunt in nobis ad ut doloribus repellat, delectus nihil laborum quas sed eum.</p>
+          </div>
           <div className="action">
-            <button className="btn-primary">Edit</button>
+            {/* <button className="btn-primary">Edit</button> */}
             <button className="btn-secondary" onClick={() => handleDelete(filteredHero.id)}>Delete</button>
           </div>
         </div>
